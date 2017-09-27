355c058a287e1faead4134b407435cc1eec98960
Robert Olson <olson@mcs.anl.gov>
2017-09-27 14:43:05 -0500

    update to 1.157

--- lib/ClientThing.pm
+++ lib/ClientThing.pm
@@ -28,7 +28,7 @@ package ClientThing;
     use POSIX;
     use HTTP::Message;
 
-    use constant AGENT_NAME => "myRAST version 1.156";
+    use constant AGENT_NAME => "myRAST version 1.157";
 
 =head1 Base Class for Server Helper Objects
 
--- lib/myRASTVersion.pm
+++ lib/myRASTVersion.pm
@@ -6,9 +6,9 @@ sub new
 {
     my($class) = @_;
     my $self = {
-	release => "1.156",
-	package_date => 1506541023,
-	package_date_str => "Sep 27, 2017 14:37:03",
+	release => "1.157",
+	package_date => 1506541374,
+	package_date_str => "Sep 27, 2017 14:42:54",
     };
     return bless $self, $class;
 }
